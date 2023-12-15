711c7711eab1e89defc2b5c0a768335d60eecccc
Trend20 <enockomondi305@gmail.com>
2023-12-15 19:37:22 +0300

    blog hero fix

--- app/Main.tsx
+++ app/Main.tsx
@@ -9,14 +9,27 @@ const MAX_DISPLAY = 5
 export default function Home({ posts }) {
   return (
     <>
+      <div className="flex h-96 w-full items-center justify-between">
+        <div className="flex w-1/2 flex-col">
+          <h1 className="flex text-4xl font-extrabold leading-9 tracking-tight">
+            Hi, I’m Enock Omondi
+          </h1>
+          <p className="mt-3 flex leading-9">
+            Welcome to my blog.I am the co-founder of DevRing, a Software Engineer by profession. In
+            my free time, I like developing side projects and blogging about them. Have a good read!
+          </p>
+        </div>
+        {siteMetadata.newsletter?.provider && (
+          <div className="flex items-center justify-center pt-4">
+            <NewsletterForm />
+          </div>
+        )}
+      </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
@@ -79,11 +92,6 @@ export default function Home({ posts }) {
           >
             All Posts &rarr;
           </Link>
-        </div>
-      )}
-      {siteMetadata.newsletter?.provider && (
-        <div className="flex items-center justify-center pt-4">
-          <NewsletterForm />
         </div>
       )}
     </>
